2c6af2e1e855296bc9efb7afeb74515ec89077bb
Christopher <crobinson@student.neumont.edu>
2016-11-18 13:56:44 -0700

    Make Dates class Day Class

--- src/edu/neumont/csc110/Final/Day.java
+++ src/edu/neumont/csc110/Final/Day.java
@@ -1,5 +1,16 @@
 package edu.neumont.csc110.Final;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Day{
+	
+	public void DateAndTime(){
+		Date currentTime = new Date();
+		SimpleDateFormat simple = new SimpleDateFormat("MM/dd/YYYY HH:mm");
+		
+		System.out.println(simple.format(currentTime));
+		
+	}
 	
 }

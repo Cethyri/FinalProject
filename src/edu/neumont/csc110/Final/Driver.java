ed76b132d67107559e7929d33b8e3c8f43cec3da
callmejuan <JReyes@student.neumont.edu>
2016-11-18 16:02:28 -0700

    AddEvent is commented out, it gets a date from user.

--- src/edu/neumont/csc110/Final/Driver.java
+++ src/edu/neumont/csc110/Final/Driver.java
@@ -5,10 +5,6 @@ package edu.neumont.csc110.Final;
 public class Driver {
 
 	public static void main(String[] args) {
-
-		//prints out the current date and time
-		Dates d = new Dates();
-		d.DateAndTime();
 		
 		
 	}

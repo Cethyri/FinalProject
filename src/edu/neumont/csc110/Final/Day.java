29ba67611b9eb3ad5b223a188c02f2ff4444dd30
callmejuan <JReyes@student.neumont.edu>
2016-11-21 11:39:14 -0700

    made getValidDateInput in Methods class.

--- src/edu/neumont/csc110/Final/Day.java
+++ src/edu/neumont/csc110/Final/Day.java
@@ -1,14 +1,11 @@
 package edu.neumont.csc110.Final;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Day {
 
-	private Date eventDate;
-	private String dateString;
 
 	SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 	Scanner in = new Scanner(System.in);
@@ -19,34 +16,16 @@ public class Day {
 		System.out.println(formatter.format(currentTime));
 	}
 
-	/*							To be moved into Calender Class
+
 	public void AddEvent() {
-		System.out.println("Please enter a date. Ex. MM/DD/YYYY");
-
-		dateString = in.nextLine();
 		
-		String regex = "(0?[1-9]|1[0-2])/(0?[1-9]|[12][0-9]|3[01])/([0-9]{4}$)"; 
-
-
-		while (!dateString.matches(regex)) {
-
-			System.out.println("Please enter a date. \nEx. MM/DD/YYYY; 1/1/1111; 01/01/1111");
-			dateString = in.nextLine();
-		}
-		
-		try {
-			eventDate = formatter.parse(dateString);
-			String newDateString = formatter.format(eventDate);
-			System.out.println(newDateString);
-		} catch (ParseException e) { }
-		
-	}*/
+	}
 
 	public void RemoveEvent() {
 
 	}
 
-	public void interact() {
+	public void interactWithEvents() {
 
 	}
 }
--- src/edu/neumont/csc110/Final/Methods.java
+++ src/edu/neumont/csc110/Final/Methods.java
@@ -1,11 +1,17 @@
 package edu.neumont.csc110.Final;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Methods {
 
 	private static Scanner in = new Scanner(System.in);
+	private static SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+	
 	private static String input;
+	private static Date dateFormat;
 
 	public Methods() {
 
@@ -26,6 +32,28 @@ public class Methods {
 		} while (!valid);
 
 		return input;
+	}
+	
+	public static void getValidDateInput(String line){
+		System.out.println("Please enter a date. Ex. MM/DD/YYYY");
+
+		line = in.nextLine();
+		
+		String regex = "(0?[1-9]|1[0-2])/(0?[1-9]|[12][0-9]|3[01])/([0-9]{4}$)"; 
+
+
+		while (!line.matches(regex)) {
+
+			System.out.println("Please enter a date. \nEx. MM/DD/YYYY; 1/1/1111; 01/01/1111");
+			line = in.nextLine();
+		}
+		
+		try {
+			dateFormat = formatter.parse(line);
+			String newDateString = formatter.format(dateFormat);
+			System.out.println(newDateString);
+		} catch (ParseException e) { }
+		
 	}
 	
 	public static void pauseOn(String line, boolean clearScreen) {

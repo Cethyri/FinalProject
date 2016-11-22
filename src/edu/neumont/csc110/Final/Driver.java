29fedfe55560d8e34331f179cfe0161a3471e3b7
Christopher <crobinson@student.neumont.edu>
2016-11-21 23:31:19 -0700

    nothing here

--- src/edu/neumont/csc110/Final/Driver.java
+++ src/edu/neumont/csc110/Final/Driver.java
@@ -1,17 +1,15 @@
 package edu.neumont.csc110.Final;
 
-
-
 public class Driver {
 
 	public static void main(String[] args) {
-//		Day D = new Day();
-//		
-//		D.AddEvent();
+		// Day D = new Day();
+		//
+		// D.AddEvent();
+
+		 Calendar C = new Calendar();
 		
-		Calendar C = new Calendar();
-		
-		C.interact();
+		 C.interact();
 	}
 
 }

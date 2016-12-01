5648db32529f2cc0592130e71f9af8167442cf2a
Christopher <crobinson@student.neumont.edu>
2016-12-01 12:06:53 -0700

    remove unused line

--- src/edu/neumont/csc110/Final/Event.java
+++ src/edu/neumont/csc110/Final/Event.java
@@ -35,7 +35,6 @@ public class Event implements Serializable {
 		this.description = description;
 		this.eventTitle = eventTitle;
 		this.eventDate = eventDate;
-		this.dateString = dateString;
 	}
 
 	public void editTitle() {

bb4bfdd02d6c5394946c6ebb60fdd113ee79b100
Christopher <crobinson@student.neumont.edu>
2016-12-01 10:54:00 -0700

    create event interaction menu

--- src/edu/neumont/csc110/Final/EventHandler.java
+++ src/edu/neumont/csc110/Final/EventHandler.java
@@ -4,51 +4,179 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 @SuppressWarnings("deprecation")
-public class EventHandler{
+public class EventHandler {
+	private static final String ADD = "day", REMOVE = "remove", EDIT = "edit", BACK = "back";
 	private Event e;
 	private SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 	private ArrayList<Event> events = new ArrayList<Event>();
-	//private ArrayList<Event> event;
-	
-	public EventHandler(){
-		
+	// private ArrayList<Event> event;
+
+	private boolean back, saved;
+
+	public EventHandler() {
+
 	}
 
-	public void veiwDay(Date start) {
-		Date currentTime = new Date();
-		System.out.println(formatter.format(currentTime));
+	public boolean veiwDay(Date d) {
+		back = false;
+
+		String input, action, title;
+		
+		String[] parts;
+
+		do {
+			input = Methods.getValidInput(interactMenu(d));
+			input = input.toLowerCase();
+
+			parts = input.split(" ");
+
+			action = parts[0];
+
+			if (parts.length > 1) {
+				title = parts[1];
+				saved = interactWith(input, title, d);
+			} else {
+				saved = interactWith(input, d);
+			}
+		} while (!back);
+
+		return saved;
+	}
+
+	private String interactMenu(Date d) {
+		String menu = "";
+		
+		ArrayList<Event> eventsOn = getEventsOn(d);
+		for (Event e : eventsOn) {
+			System.out.println(e);
+			System.out.println();
+		}
+		
+		menu += "What would you like to do?";
+		menu += "\t\t";
+		// menu += "Current calendar: " + name;
+		menu += "\n\n";
+		menu += "\tInput - result\n\n";
+		menu += "\t" + ADD + " - add a new events\n";
+		menu += "\t" + REMOVE + " (Event title) - remove an event\n";
+		menu += "\t" + EDIT + " (Event title) - edit an event\n";
+		menu += "\t" + BACK + " - return to the file menu\n";
+		return menu;
+	}
+
+	private boolean interactWith(String action, String title, Date d) {
+
+		Methods.clearScreen();
+
+		switch (action) {
+		case REMOVE:
+			removeEvent(title, d);
+			saved = false;
+			break;
+		case EDIT:
+			editEvent(title, d);
+			saved = false;
+			break;
+		default:
+			Methods.pauseOn("Please enter an input provided.", true);
+		}
+
+		return saved;
 	}
 	
-	public void addEvent() {
+	private boolean interactWith(String action, Date d) {
+
+		Methods.clearScreen();
+
+		switch (action) {
+		case ADD:
+			addEvent(d);
+			saved = false;
+			break;
+		case BACK:
+			back = true;
+			break;
+		default:
+			Methods.pauseOn("Please enter an input provided.", true);
+		}
+
+		return saved;
+	}
+	
+	public void addEvent(Date d) {
 		e = new Event();
 		e.editAll();
 		e.displayAll();
 		events.add(e);
-		System.out.println(events);
 	}
 
-	public void removeEvent() {
+	public void removeEvent(String title, Date d) {
 		events.remove(0);
 		System.out.println("Theres nothing there. " + events);
 	}
 	
-	public ArrayList<Event> getEventsOnDay(Date d) {
+	public void editEvent(String title, Date d) {
+		
+	}
+
+	public int getEventAmount(Date d) {
+		return getEventsOn(d).size();
+	}
+
+	public String getPriorities(Date d) {
+		String priorities = "";
+		boolean low = false, med = false, hi = false;
+		ArrayList<Event> eventsOnDay = getEventsOn(d);
+
+		for (Event e : eventsOnDay) {
+			switch (e.getEventPriority()) {
+			case LOW:
+				low = true;
+				break;
+			case MEDIUM:
+				med = true;
+				break;
+			case HIGH:
+				hi = true;
+				break;
+			default:
+			}
+		}
+
+		if (low) {
+			priorities += "L";
+		}
+		if (med) {
+			priorities += "M";
+		}
+		if (hi) {
+			priorities += "H";
+		}
+
+		return priorities;
+	}
+
+	public ArrayList<Event> getEventsOn(Date d) {
 		Date temp;
-		ArrayList<Event> eventsOnDay = new ArrayList<Event> ();
+		ArrayList<Event> eventsOnDay = new ArrayList<Event>();
 		for (Event e : events) {
 			temp = e.getDate();
 			if (temp.getMonth() == d.getMonth() && temp.getDate() == d.getDate() && temp.getYear() == d.getYear()) {
 				eventsOnDay.add(e);
 			}
-//			else if (e.reoccursOn(d)) {
-//				eventsOnDay.add(e);
-//			}
+			// else if (e.checkReoccursOn(d)) {
+			// eventsOnDay.add(e);
+			// }
 		}
 		return (ArrayList<Event>) events;
 	}
-	
+
 	public ArrayList<Event> getEvents() {
 		return (ArrayList<Event>) events;
 	}
 	
+	public void addEvent(Event event) {
+		events.add(event);
+	}
 }
+
